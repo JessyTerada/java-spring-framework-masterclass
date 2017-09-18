b3bf18d6a39bfa5e365458c1066d0bba393cf39a
Jessica Tereda (Financial Services) <JE385653@wipro.com>
2017-09-18 17:49:27 -0300

    class 9 complete

--- udemy-hibernate-spring/src/main/java/com/mastercard/spring/TennisCoach.java
+++ udemy-hibernate-spring/src/main/java/com/mastercard/spring/TennisCoach.java
@@ -1,10 +1,14 @@
 package com.mastercard.spring;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach{
 
 	//define a setter method 
@@ -16,6 +20,17 @@ public class TennisCoach implements Coach{
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}	
+	
+	//define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	
+	//define my destroy method
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+	}
 
 	@Override
 	public String getDailyWorkout() {
