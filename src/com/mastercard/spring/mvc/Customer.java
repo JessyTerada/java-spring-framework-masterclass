b11d195fbe8ec063ef4be4d0843cc6ea1f7d59e2
Jessica Tereda (Financial Services) <JE385653@wipro.com>
2017-09-21 17:55:34 -0300

    class 15 incomplete

--- src/com/mastercard/spring/mvc/Customer.java
+++ src/com/mastercard/spring/mvc/Customer.java
@@ -1,6 +1,9 @@
 package com.mastercard.spring.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -11,16 +14,42 @@ public class Customer {
 	@Size(min=1, message=" is required")
 	private String lastName;
 	
+	@Min(value=0, message="must be great than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private int postalCode;
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+	
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
 	}
 }
